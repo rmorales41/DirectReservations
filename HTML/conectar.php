75c6466f3b2b50754a84a51420d7ff2ff4aa5c90
rmorales <rmorales41@hotmail.com>
2022-06-14 17:07:12 -0600

    pruebas de login

--- HTML/conectar.php
+++ HTML/conectar.php
@@ -1,56 +1,20 @@
 <?php
+class Cconexion{
+    function Conexiondb(){
+        $host       ='server-ti';
+        $dbname     ='Usuarios';
+        $username   ='root';
+        $password    = '';
+        $puerto  = '1433';
 
-
-$hostname   = "SERVER-TI";
-$usuariodb  = "root";
-$passworddb = "";
-$dbnombre   = "Usuarios";
-
-$conn = sqlsrv_connect($hostname,$usuariodb,$passworddb,$dbnombre);
-
-if($conn){
-    echo "conexion establecida.<br/>";
-
-}else{
-    echo "Conexion no se pudo establecer.<br/>";
-    die(print_r(sqlsrv_errors(),true));
-
+        try{
+            $conn = new PDO ("sqlsrv:Server=$host,$puerto;DataBase=$dbname",)$username,$password);
+            echo "Se Conecto a la base de datos "
+        }
+        cat(PDOException $exp){
+            echo ("No se logro conectar correctamente a la base de datos: $dbname ,error: $exp");
+        }
+        return $conn;
+    }
 }
-
-
-
-//include_once('base_datos.php');
-
-// se recibe los datos de ingreso 
-$Usuario=$_POST['Usuario'];
-$Contrasena=$_POSTt['Contrasena'];
-
-echo "Revisa info <br>";
-echo "$Usuario,$Contrasena <br>";
-
-$conectar=conn(); // ejecta conexion
-if (!$conn)
-{
-    die("No hay Conexion".mysqcli_connect_error());
-}
-
-$sql="select * from usuarios where Usuario = '".$Usuario"' and Clave = '".$Contrasena."'";
-$result = mysqli_query($conectar,$sql) or trigger_error("Query Failed! Sql - Error".mysqli_error($conectar),E_User_error);
-
-echo "$sql";
-
-if ($result==1)
-{
-    // ingreso 
-    echo ="Ingreso".$usuario ;
-
-}
-else if($nr==0)
-{
-    
-    (header("location:login.html"));
-    
-    
-}
-
 ?>
