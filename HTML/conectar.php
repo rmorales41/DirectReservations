da717e54aa86d270bb3a6b48618eb519a11db162
rmorales <rmorales41@hotmail.com>
2022-06-14 15:27:53 -0600

    Incluir cambios de login a la rama desarrollo

--- HTML/conectar.php
+++ HTML/conectar.php
@@ -1,5 +1,6 @@
 <?php
 include_once('base_datos.php');
+
 // se recibe los datos de ingreso 
 $Usuario=$_POST['Usuario'];
 $Contrasena=$_POSTt['Contrasena'];
@@ -8,10 +9,28 @@ echo "Revisa info <br>";
 echo "$Usuario,$Contrasena <br>";
 
 $conectar=conn(); // ejecta conexion
-$sql="select * from usuarios where Usuario = $Usuario and Clave =$Contrasena ";
+if (!$conn)
+{
+    die("No hay Conexion".mysqcli_connect_error());
+}
+
+$sql="select * from usuarios where Usuario = '".$Usuario"' and Clave = '".$Contrasena."'";
 $result = mysqli_query($conectar,$sql) or trigger_error("Query Failed! Sql - Error".mysqli_error($conectar),E_User_error);
 
 echo "$sql";
 
+if ($result==1)
+{
+    // ingreso 
+    echo ="Ingreso".$usuario ;
+
+}
+else if($nr==0)
+{
+    
+    (header("location:login.html"))
+    
+    
+}
 
 ?>
